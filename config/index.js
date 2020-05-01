84814220873b4b8e7fb150ac972734f1ec19526b
bxm0927 <80583600@qq.com>
2020-05-01 12:14:49 +0800

    baseConfig

--- config/index.js
+++ config/index.js
@@ -1,6 +1,19 @@
+/*
+ * Server Configuration
+ * @Author: xiaoming.bai
+ * @Date: 2020-05-01 12:13:00
+ * @Last Modified by: xiaoming.bai
+ * @Last Modified time: 2020-05-01 12:13:54
+ */
+
 const devConfig = require('./config.dev')
 const prodConfig = require('./config.prod')
 
 const isDev = process.env.NODE_ENV !== 'production'
+const config = isDev ? devConfig : prodConfig
 
-module.exports = isDev ? devConfig : prodConfig
+const baseConfig = {
+  // ...
+}
+
+module.exports = { ...baseConfig, ...config }
--- server/global/index.js
+++ server/global/index.js
@@ -3,11 +3,11 @@
  * @Author: xiaoming.bai
  * @Date: 2020-05-01 01:37:16
  * @Last Modified by: xiaoming.bai
- * @Last Modified time: 2020-05-01 11:29:24
+ * @Last Modified time: 2020-05-01 12:10:31
  */
 
-const http = require('../../http')
-const logger = require('../../logger')
+const http = require('./http')
+const logger = require('./logger')
 const config = require('../../config')
 
 class Lib {
